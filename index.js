5eb1ec254002599e68727cd6896c63e91f81567d
Aravind S <saravind23399@gmail.com>
2019-02-22 14:43:02 +0530

    Integration with Web Server

--- index.js
+++ index.js
@@ -3,6 +3,36 @@ const express = require('express')
 const PORT = 3000
 
 const app = express()
+const SerialPort = require('serialport');
+const Readline = require('@serialport/parser-readline');
+const serialPORT = '/dev/ttyACM0'
+
+const port = new SerialPort(serialPORT, { baudRate: 9600});
+
+const parser = port.pipe(new Readline({ delimiter: '\n' }));
+
+port.on("open", () => {
+    console.log('Serial Port '+serialPORT+' now OPEN');
+  });
+
+  parser.on('data', (data)=>{
+      console.log("Received : " + data)
+  })
+
+app.get('/test/:command',(req, res)=>{
+
+port.flush((error)=>{
+    port.write(req.params.command,(error, count)=>{
+        if(error){
+          console.log(error)
+        } else {
+            console.log("Message transmitted Successfully!!!")
+        }
+      })
+});
+
+res.send('OKAY')
+})
 
 app.listen(PORT, ()=>{
     console.log('Server Started on Port ' + PORT)
